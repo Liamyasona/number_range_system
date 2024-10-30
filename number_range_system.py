b573d830842b466c765d07af96e3acaf0a4c6985
unknown <liamkyleyasona@gmail.com>
2024-10-30 19:35:44 +0800

    Finally working: problem was forgot calling the function below

--- number_range_system.py
+++ number_range_system.py
@@ -1,7 +1,8 @@
 # Define the ranges
 def number_inputs():
     # Make a list on each range to store the inputted numbers   
-    ranges = [0 for i in range(5)]
+    ranges = [0, 0, 0, 0, 0]
+
 
     # Loop to keep asking the user to input a number
     while True:
@@ -10,17 +11,18 @@ def number_inputs():
             number_input = int(input("Please input a number ranging from 1 to 50: "))
 
             # Conditions on what are considered valid for the user to input
-            if 1 <= number_input <= 50:        
-                if number_input > 1 and number_input < 10:
-                    ranges[0] += 1 
-                elif number_input > 11 and number_input < 20:
-                    ranges[0] += 1 
-                elif number_input > 21 and number_input < 30:
-                    ranges[0] += 1 
-                elif number_input > 31 and number_input < 40:
-                    ranges[0] += 1 
-                elif number_input > 41 and number_input < 50:
-                    ranges[0] += 1 
+            if 1 <= number_input <= 50:
+              
+                if number_input >= 1 and number_input <= 10:
+                    ranges[0] += 1 #
+                elif number_input >= 11 and number_input <= 20:
+                    ranges[1] += 1 
+                elif number_input >= 21 and number_input <= 30:
+                    ranges[2] += 1 
+                elif number_input >= 31 and number_input <= 40:
+                    ranges[3] += 1 
+                elif number_input >= 41 and number_input <= 50:
+                    ranges[4] += 1 
             else:
                 print("Invalid number! Displaying previous numbers.")
                 break # When user inputs a invalid number, the loop breaks and prints the numbers
@@ -28,7 +30,11 @@ def number_inputs():
         # If user inputted a invalid integer
         except ValueError:
             print("Invalid integer! Please print a valid integer.")
+    
+
 
 
 # Ask the user to input again
 # If user input is invalid, print the inputted numbers in their following ranges
+
+number_inputs()

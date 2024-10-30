47acd1eaf63ae966579cf3402e3028bc62ecf536
unknown <liamkyleyasona@gmail.com>
2024-10-30 17:44:48 +0800

    If-Else and Try & Except conditions for the loop

--- number_range_system.py
+++ number_range_system.py
@@ -3,15 +3,22 @@ def number_inputs():
     # Make a list on each range to store the inputted numbers   
     ranges = [0 for i in range(5)]
 
-# Ask user to input numbers ranging from 1 to 50
+# Loop to keep asking the user to input a number
     while True:
-        number_input = int(input("Please input a number ranging from 1 to 50: "))
+        try:
+            # Ask user to input numbers ranging from 1 to 50
+            number_input = int(input("Please input a number ranging from 1 to 50: "))
 
-        if 1 <= number_input <= 50:
-            
-        elif:
-            print("Invalid number! Displaying previous numbers.")
-            break
+            # Conditions on what are considered valid for the user to input
+            if 1 <= number_input <= 50:
+                
+            else:
+                print("Invalid number! Displaying previous numbers.")
+                break # When user inputs a invalid number, the loop breaks and prints the numbers
+        
+        # If user inputted a invalid integer
+        except ValueError:
+            print("Invalid integer! Please print a valid integer.")
 
 
 # Ask the user to input again

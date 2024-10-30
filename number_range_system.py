954db0da9531e1ff5f477682a9c5ddd99c365d02
unknown <liamkyleyasona@gmail.com>
2024-10-30 16:30:57 +0800

    Start code

--- number_range_system.py
+++ number_range_system.py
@@ -1,4 +1,15 @@
 # Define the ranges
+def number_inputs():
+    
+
+print("Welcome to the system!")
 # Ask user to input numbers ranging from 1 to 50
+while True:
+    number = int(input("Please input a number ranging from 1 to 50: "))
+
+    if 50 > number < 0:
+        print("Invalid number! Displaying previous numbers.")
+
+
 # Ask the user to input again
 # If user input is invalid, print the inputted numbers in their following ranges

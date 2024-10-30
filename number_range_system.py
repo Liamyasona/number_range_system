c231c027a143e45ddb2cfcb2d65bcba2d4cd89b8
unknown <liamkyleyasona@gmail.com>
2024-10-30 14:50:51 +0800

    First commit: pseudocode

---
+++ number_range_system.py
@@ -0,0 +1,4 @@
+# Define the ranges
+# Ask user to input numbers ranging from 1 to 50
+# Ask the user to input again
+# If user input is invalid, print the inputted numbers in their following ranges
